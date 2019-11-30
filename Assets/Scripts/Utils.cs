cb140b0802de7c31adf8dfdae442670a4c4a4034
Yisel FY <brancyxy@gmail.com>
2019-11-30 13:27:40 +0100

    Alap menü
    szar a grafikám tudom
    
    a tilesetes cucc majd a simulation alá kell

---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utils : MonoBehaviour
+{
+    [SerializeField]
+    GameObject menu;
+    [SerializeField]
+    GameObject simulation;
+
+    public void Begin()
+    {
+        menu.SetActive(false);
+        simulation.SetActive(true);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
