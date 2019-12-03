a13c1034f981db9d761c8cf47f60f4ba1ecaa9ea
8dave8 <43006387+8dave8@users.noreply.github.com>
2019-12-03 09:38:07 +0100

    tilecucc

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 using UnityEngine.Tilemaps;
 public class GameManager : MonoBehaviour
 {
+    public Sprite[] Sprites; 
     public GameObject Buttons;
     public Tile[] Tiles;
     public Tilemap highlightMap;
     private Vector3Int previous;
     private int Lerakando;
+    private Vector3Int tmp;
     // Start is called before the first frame update
     void Start()
     {
+        tmp = new Vector3Int(0,0,0);
         Buttons.SetActive(false);
     }
-
+    
     // Update is called once per frame
     void Update()
     {
@@ -22,13 +25,24 @@ public class GameManager : MonoBehaviour
         {
             Buttons.SetActive(true);
             Touch touch = Input.GetTouch(0);
+            /* 
             Vector3Int currentCell = highlightMap.WorldToCell(touch.rawPosition);
             //currentCell.x += 1;  
-            
             highlightMap.SetTile(currentCell,Tiles[Lerakando]);
-            highlightMap.RefreshAllTiles();
+            */
             //setactive valaszto menu
-            Debug.Log(currentCell+"  "+Lerakando);
+            
+            Ray ray = Camera.main.ScreenPointToRay(touch.position);
+            Vector3 worldPoint = ray.GetPoint(-ray.origin.z / ray.direction.z);
+            Vector3Int position = highlightMap.WorldToCell(worldPoint);
+            Debug.Log(position);
+            if(position.x < -2) position = tmp;// nemenged belenyulni a kiirasba
+            else tmp = position; 
+            TileBase tile = highlightMap.GetTile(position);
+            Tiles[Lerakando].sprite = Sprites[Lerakando];
+            highlightMap.SetTile(position, Tiles[Lerakando]);
+            highlightMap.RefreshTile(position);
+            Debug.Log(position+" "+ Tiles[Lerakando]);
         }
     }
     public void lerakando(int id)
