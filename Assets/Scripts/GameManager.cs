b2b31c9b83e4e20a6c2ea4db201e6c68868eb1a7
Yisel FY <brancyxy@gmail.com>
2019-12-05 01:34:16 +0100

    még több bugfix
    már csak grafika van hátra meg az h átnézzétek ezt de elvileg már jó

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] GameObject simulation;
@@ -17,10 +16,10 @@ public class GameManager : MonoBehaviour
     public Text vizszint;
     public GameObject deathScene;
     public Text DeathText;
-    
+    private bool alive;
 
 
-    private Vector3Int elozoLerakotEpitmenyHelyeKoordinatakban;
+    private List<Vector3Int> elozoLerakotEpitmenyekHelyeKoordinatakban; //donmai, kore namae wa meme desu
     //epites
     public int[] buildings;
     public Sprite[] Sprites;
@@ -40,18 +39,19 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        alive = true;
         Lerakando = 4;
         vizSzint = 50;
         buildings = new int[4];
         tmp = new Vector3Int(0,0,0);
-        elozoLerakotEpitmenyHelyeKoordinatakban = tmp;
+        elozoLerakotEpitmenyekHelyeKoordinatakban = new List<Vector3Int>();
         Buttons.SetActive(false);
     }
     
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (simulation.activeSelf)
+        if (simulation.activeSelf && alive)
         {
             Epites();
             runTime();
@@ -60,8 +60,12 @@ public class GameManager : MonoBehaviour
 
     private void runTime()
     {
-        if (vizSzint > 10 && vizSzint < 90)
+        if (!(vizSzint > 10 && vizSzint < 90))
         {
+            DeathText.text = termeltKWH + " kW";
+            deathScene.SetActive(true);
+            alive = false;
+        }
             vizSzint += vizbeszabalyzo.value * Time.deltaTime;
             vizSzint -= buildings[0] * Time.deltaTime; 
             KWH = 0;
@@ -70,16 +74,10 @@ public class GameManager : MonoBehaviour
             KWH += (buildings[0] * 10) * (buildings[2] * 2f);
             KWH += (buildings[0] * 10) * (buildings[3] * 5);
             termeltKWH += KWH * Time.deltaTime;
-            TXtermeltAram.text = KWH + " kW/h";
-            TXtermeltKWH.text = termeltKWH + " kW";
+            TXtermeltAram.text = "Termelés: " + KWH + "kW/h";
+            TXtermeltKWH.text = "kitermelt áram: " + Mathf.Round(termeltKWH) + "kW";
             vizszint.text = "vizszint: "+Mathf.Round(vizSzint*10)/10+" %";
             slidertext.text = Mathf.Round(vizbeszabalyzo.value*10)/10+"";
-        }
-        else {
-            Time.timeScale = 0f;
-            deathScene.SetActive(true);
-            DeathText.text = termeltKWH + " kW";
-        }
     }
 
     private void Epites()
@@ -90,12 +88,12 @@ public class GameManager : MonoBehaviour
             Ray ray = Camera.main.ScreenPointToRay(touch.position);
             Vector3 worldPoint = ray.GetPoint(-ray.origin.z / ray.direction.z);
             Vector3Int position = highlightMap.WorldToCell(worldPoint);
-            if (position.x < -1.5f) position = tmp; // nemenged belenyulni a kiirasba
+            if (position.x < -1.5f) return; // nemenged belenyulni a kiirasba
             else tmp = position;
-            //Debug.Log(worldPoint);
+            Debug.Log(worldPoint);
         }
         if (selectionIsActive) Buttons.SetActive(true);
-        if (Buttons.activeSelf && Lerakando != 4 && elozoLerakotEpitmenyHelyeKoordinatakban != tmp) { 
+        if (Buttons.activeSelf && Lerakando != 4 && !elozoLerakotEpitmenyekHelyeKoordinatakban.Contains(tmp)) { 
                 buildings[Lerakando]++; 
                 lerakandoSprite.GetComponent<SpriteRenderer>().sprite = Sprites[Lerakando];
                 Instantiate(lerakandoSprite, tmp, Quaternion.identity);
@@ -103,7 +101,7 @@ public class GameManager : MonoBehaviour
                 Buttons.SetActive(false);
                 buildBT.SetActive(true);
                 Lerakando = 4;
-            elozoLerakotEpitmenyHelyeKoordinatakban = tmp;
+            elozoLerakotEpitmenyekHelyeKoordinatakban.Add(tmp);
         }
     }
 
